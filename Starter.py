c2b58676eb99217c2bb870f5dfed408b7415b280
MASTERVO2021 <lzh3871@stacmail.net>
2026-02-05 12:04:33 +1300

    Using print
    Helping with print ()

---
+++ Starter.py
@@ -0,0 +1,8 @@
+'''This program demostrates print(), data types, variables, unput and f-strings'''
+
+#print() is a function that outputs whatever is inside the brackets
+#numbers can be included directly in the brackets
+print(123)
+
+#when printing text, it must be in speechmarks which turn into a string
+print("hello")
