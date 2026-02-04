7ba9f35f3703c83d8cac79dc0b1a3abe56dfad78
MASTERVO2021 <lzh3871@stacmail.net>
2026-02-05 12:34:33 +1300

    intro to python basics

--- Starter.py
+++ Starter.py
@@ -6,3 +6,27 @@ print(123)
 
 #when printing text, it must be in speechmarks which turn into a string
 print("hello")
+
+#there are lots of different types of data types
+#Intigers, decimals (floating point numbers)
+#text (strings), boolean (true or false)
+
+#use variable to store information
+name = "Pluto"
+first_name = "John"
+last_name = "Smith"
+age = 24
+
+#you can inclise variables inside print() statements
+print(name)
+#To combine variables with a string, we use f-string
+#the variable goes inside curly brackets {}
+print(f"my dog is called {name} and he is {age} years old")
+
+#we use input() to get input from the use
+user_name = input("What is your name?")
+print(f"hello {user_name}")
+
+#Print hello to the user and ask them how old they are
+print(f"Hello {user_name}")
+input("how old are you?")
