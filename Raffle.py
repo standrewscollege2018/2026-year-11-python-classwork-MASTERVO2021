90306ad03f1c146aa0dcb9dee15b004450b4c692
MASTERVO2021 <lzh3871@stacmail.net>
2026-03-11 10:29:20 +1300

    work

--- Raffle.py
+++ Raffle.py
@@ -1,14 +1,15 @@
 '''Raffle'''
 
 people = []
+amount = int(0)
 import random
 Asking = True
 while Asking == True:
     Person = input("Enter a name")
     people.append(Person)
+    amount + int(1)
     if Person == "end":
         Asking = False
-random
-    
 
+print(amount)
 
---
+++ Test.py
@@ -0,0 +1,17 @@
+#Making the intro sections before the questions start
+name = input("What is your name?")
+print("There will be 10 questions")
+guess = input("How many questions do you think you will get right")
+score = int(0)
+
+#Making questions
+print("Question 1: Does pineapple belong on pizza?")
+q1 = input("")
+if q1 = "Yes":
+    print("You are right")
+else:
+    print("GET OUT")
+print("Question 2: Is tomato a fruit or vegtable?")
+q2 = input("")
+print("Question 3: ")
+q3 = input("")
