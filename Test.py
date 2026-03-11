778041da3c695a9c781a4a825c7c17acb4a599f9
MASTERVO2021 <lzh3871@stacmail.net>
2026-03-12 12:33:22 +1300

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,15 +3,27 @@ name = input("What is your name?")
 print("There will be 10 questions")
 guess = input("How many questions do you think you will get right")
 score = int(0)
-
+answers = ["Yes", "Fruit", "Before", "cereal"]
+questions = ["Question 1: Does pineapple belong on pizza?","Question 2: Is tomato a fruit or vegtable?","Question 3: Do you brush your teeth before or after you eat?","Do you pour milk or cereal first?"]
+quiz = True
 #Making questions
-print("Question 1: Does pineapple belong on pizza?")
+
+print(questions [0])
 q1 = input("")
-if q1 = "Yes":
-    print("You are right")
-else:
-    print("GET OUT")
-print("Question 2: Is tomato a fruit or vegtable?")
-q2 = input("")
-print("Question 3: ")
-q3 = input("")
+print("The correst answer is yes")
+
+print(questions [1])
+q1 = input("")
+print("The correst answer is fruit")
+
+print(questions [2])
+q1 = input("")
+print("The correst answer is before")
+
+print(questions [3])
+q1 = input("")
+print("The correst answer is cereal")
+
+
+
+
