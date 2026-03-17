ce716fe017eab2a63a75b8f2668b320da46a2adb
MASTERVO2021 <lzh3871@stacmail.net>
2026-03-18 10:28:30 +1300

    test

--- Lists.py
+++ Lists.py
@@ -1,7 +1,14 @@
-'''Lists'''
-
-people = ["Abbey", "Bob", "Connor"]
-status = ["No licence", "Learners licence", "Full licence"]
-update = int(1)
-print("Student driver status")
-print(int("=" + 23))
+ka = True
+answers = ["Yes", "Fruit", "Before", "cereal", "No scope", "Yes", "No", "Yes", "No", "Yes"]
+questions = ["Question 1: Does pineapple belong on pizza?","Question 2: Is tomato a fruit or vegtable?","Question 3: Do you brush your teeth before or after you eat?","Question 4: Do you pour milk or cereal first?", "Question 5: What comes after 360?", "Question 6: Is this quiz good?", "Question 7: Is 67 the best number?", "Question 8: Is starcraft a dead game?", "Question 9: Do you wear socks with sandals/slippers? (yes no question)", "Question 10: Am i good at making quizes?"]
+score = 0
+while ka == True:
+    print(questions [0])
+    q1 = input("")
+    if q1 == answers [0]:
+        print("Correct")
+        score += 1
+    else:
+        print("Wrong")
+    answers += 1
+    questions += 1
