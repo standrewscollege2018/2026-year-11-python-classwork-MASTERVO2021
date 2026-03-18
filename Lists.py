8cc537a72158b385047e616c3cd8f2d976a40a95
MASTERVO2021 <lzh3871@stacmail.net>
2026-03-19 12:35:45 +1300

    Work

--- Lists.py
+++ Lists.py
@@ -1,14 +1,7 @@
 ka = True
-answers = ["Yes", "Fruit", "Before", "cereal", "No scope", "Yes", "No", "Yes", "No", "Yes"]
-questions = ["Question 1: Does pineapple belong on pizza?","Question 2: Is tomato a fruit or vegtable?","Question 3: Do you brush your teeth before or after you eat?","Question 4: Do you pour milk or cereal first?", "Question 5: What comes after 360?", "Question 6: Is this quiz good?", "Question 7: Is 67 the best number?", "Question 8: Is starcraft a dead game?", "Question 9: Do you wear socks with sandals/slippers? (yes no question)", "Question 10: Am i good at making quizes?"]
-score = 0
 while ka == True:
-    print(questions [0])
-    q1 = input("")
-    if q1 == answers [0]:
-        print("Correct")
-        score += 1
+    guess = int((input("How many questions do you think you will get right (Put capital letters at the start of you answer)")))
+    if guess > 10 or guess < 0:
+        print("That is not a number between 0 to 10. Grow a brain")
     else:
-        print("Wrong")
-    answers += 1
-    questions += 1
+        ka = False
--- Test.py
+++ Test.py
@@ -1,1 +1,48 @@
-print("Hello")
+#Making variables for later
+ka = True
+score = 0
+answers = ["Yes", "Fruit", "Before", "Cereal", "No scope", "Yes", "No", "Yes", "No", "Yes"]
+questions = ["Question 1: Does pineapple belong on pizza?","Question 2: Is tomato a fruit or vegtable?","Question 3: Do you brush your teeth before or after you eat?","Question 4: Do you pour milk or cereal first?", "Question 5: What comes after 360?", "Question 6: Is this quiz good?", "Question 7: Is 67 the best number?", "Question 8: Is starcraft a dead game?", "Question 9: Do you wear socks with sandals/slippers? (yes no question)", "Question 10: Am i good at making quizes?"]
+quiz = True
+qn = 0
+an = 0
+#Getting their name
+print("What is your name?")
+name = input("")
+if name == "":
+    print("Name cannot be blank. Insert your name")
+    name = input("")
+#Asking for questions
+print("There will be 10 questions")
+#Getting a valid number guess
+while ka == True:
+    guess = int((input("How many questions do you think you will get right (Put capital letters at the start of you answer)")))
+    if guess > 10 or guess < 0:
+        print("That is not a number between 0 to 10. Grow a brain")
+    else:
+        ka = False
+ka = True
+#Printing questions
+while ka == True:
+    print(questions [qn])
+    q1 = input("")
+    if q1 == answers [an]:
+        print("Correct")
+        score += 1
+    else:
+        print("Wrong")
+        print(f"The correct answer was {answers [an]}")
+    qn += 1
+    an += 1
+    if qn == 10:
+        ka = False
+#Comparing the score
+if guess < score:
+    print(f"Congratulations {name}, Your score was higher than your guess, which was {guess}. You got {score}/10")
+if score > guess:
+    print(f"Sorry {name}, Your score was lower than your guess, which was {guess}. You got {score}/10")
+if score == guess:
+    print(f"Congratulations {name}, Your score was the same as your guess, which was {guess}. You got {score}/10")
+
+
+
